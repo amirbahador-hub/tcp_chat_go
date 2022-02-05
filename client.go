e5ae2f696e789e1bd77e00550004bca8d58b5c74
amirbahador.pv <amirbahador.pv@gmail.com>
2022-02-05 10:53:59 -0500

    get program output go build .

--- client.go
+++ client.go
@@ -27,42 +27,42 @@ func (c *client) readInput() {
 
 		msg = strings.Trim(msg, "\r\n")
 
-		args := strings.Split(msg, "")
+		args := strings.Split(msg, " ")
 		cmd := strings.TrimSpace(args[0])
 
 		switch cmd {
-			case "/nick": 
-				c.commands <- command {
-					id:			CMD_NICK,
-					client:		c,
-					args:		args,
-				}
-			case "/join":
-				c.commands <- command {
-					id:			CMD_JOIN,
-					client:		c,
-					args:		args,
-				}
-			case "/rooms":
-				c.commands <- command {
-					id:			CMD_ROOMS,
-					client:		c,
-					args:		args,
-				}
-			case "/msg":
-				c.commands <- command {
-					id:			CMD_MSG,
-					client:		c,
-					args:		args,
-				}
-			case "/quit":
-				c.commands <- command {
-					id:			CMD_QUIT,
-					client:		c,
-					args:		args,
-				}
-			default:
-				c.err(fmt.Errorf("unknown command: %s", cmd))
+		case "/nick": 
+			c.commands <- command {
+				id:			CMD_NICK,
+				client:		c,
+				args:		args,
+			}
+		case "/join":
+			c.commands <- command {
+				id:			CMD_JOIN,
+				client:		c,
+				args:		args,
+			}
+		case "/rooms":
+			c.commands <- command {
+				id:			CMD_ROOMS,
+				client:		c,
+				args:		args,
+			}
+		case "/msg":
+			c.commands <- command {
+				id:			CMD_MSG,
+				client:		c,
+				args:		args,
+			}
+		case "/quit":
+			c.commands <- command {
+				id:			CMD_QUIT,
+				client:		c,
+				args:		args,
+			}
+		default:
+			c.err(fmt.Errorf("unknown command: %s", cmd))
 		
 		}
 	}
