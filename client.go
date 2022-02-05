36345777bee67080a1aa9a2661c75fd6a12ee77c
amirbahador.pv <amirbahador.pv@gmail.com>
2022-02-05 09:52:02 -0500

    Client send msg to channel commands

--- client.go
+++ client.go
@@ -31,11 +31,36 @@ func (c *client) readInput() {
 		cmd := strings.TrimSpace(args[0])
 
 		switch cmd {
-			case "/nick":
+			case "/nick": 
+				c.commands <- command {
+					id:			CMD_NICK,
+					client:		c,
+					args:		args,
+				}
 			case "/join":
+				c.commands <- command {
+					id:			CMD_JOIN,
+					client:		c,
+					args:		args,
+				}
 			case "/rooms":
+				c.commands <- command {
+					id:			CMD_ROOMS,
+					client:		c,
+					args:		args,
+				}
 			case "/msg":
+				c.commands <- command {
+					id:			CMD_MSG,
+					client:		c,
+					args:		args,
+				}
 			case "/quit":
+				c.commands <- command {
+					id:			CMD_QUIT,
+					client:		c,
+					args:		args,
+				}
 			default:
 				c.err(fmt.Errorf("unknown command: %s", cmd))
 		
