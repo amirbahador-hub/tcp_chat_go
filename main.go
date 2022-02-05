42e9f460d836321d95c8b13bd6bc34b013d8f9ea
amirbahador.pv <amirbahador.pv@gmail.com>
2022-02-05 09:30:06 -0500

    Add main

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net"
+	"log"
+)
+
+func main() {
+	s := newServer()   // initial server
+
+	listener, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatalf("unable to start server : %s", err.Error())
+	}
+
+	defer listener.Close()
+	log.Printf("start server on :8888")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("unable to accept connection: %s", err.Error())
+			continue
+		}
+
+		go s.newClient(conn)
+	}
+
+}
